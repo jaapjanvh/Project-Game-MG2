bbddb55ec174e90ba6ace34927fb985353306116
Bram Brouwer <Bram Brouwer>
2016-12-17 12:54:24 +0100

    voortuig spawnt random gebaseerd op aanwezige kleuren

--- add_vehicle.java
+++ add_vehicle.java
@@ -8,35 +8,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class add_vehicle extends Actor
 {
-    public static int vmg2_test123  = 2;
+    public static int vmg2_addvehicle  = 2;
     /**
-     * Act - do whatever the test wants to do. This method is called whenever
+     * Act - do whatever the add_vehicle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
         addnewvehicel();
-        System.out.println("BLUE: " + vehicle.v_blue +"RED: "+ vehicle.v_red +"GREEN: "+ vehicle.v_green);
     }
-    
+    /**
+     * Adds a vehicle to the world. 
+     * Starts with 2. Then after a vehicle is removed, vmg2_test is set to 1 and it triggers this funtion to add a new vehicle.
+     */
     public void addnewvehicel()
     {
          if (vehicle.v_blue > 0 || vehicle.v_red > 0 || vehicle.v_green > 0)
          {
-         //System.out.println("TEST1 "+ vmg2_test123);
-             if (vmg2_test123 == 2)
+             if (vmg2_addvehicle == 2)
              {
                  vehicle vehicle = new vehicle();
                  getWorld().addObject(vehicle,575,610);
-                 vmg2_test123 = 0;
+                 vmg2_addvehicle = 0;
              }
-             if (vmg2_test123 == 1)
+             if (vmg2_addvehicle == 1)
                  {
                  vehicle vehicle = new vehicle();
                  getWorld().addObject(vehicle,575,610);
-                 vmg2_test123 = 0;
+                 vmg2_addvehicle = 0;
                  }
-           // System.out.println("TEST2 "+ vmg2_test123);
+        }
+        else
+        {
+            World myWorld = getWorld();
+            GameOver gameover = new GameOver();
+            myWorld.addObject(gameover, myWorld.getWidth()/2, myWorld.getHeight()/2);
+            Greenfoot.stop();           
         }
     }
 }
--- vehicle.java
+++ vehicle.java
@@ -1,24 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class vehicle here.
+ * Vehicle class.
+ * Spawns in a random vehicle as long as there are containers in the world.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @BramBrouwer
+ * 
  */
 public class vehicle extends add_vehicle
 {
-    public int vTemp = 1;
-    private int vRandom = Greenfoot.getRandomNumber(1);
+    public int vRandom = Greenfoot.getRandomNumber(3);
+    public int vRandom1 = Greenfoot.getRandomNumber(2);    
     private String vColor = "blank";
-    private String vContainer = "Empty";
-    //MyWorld world = (MyWorld)getWorld();
-    public static int v_blue = 1;
+    public static int v_blue = 3;
     public static int v_red = 2;
-    public static int v_green = 1;
-    GreenfootImage image = getImage(); 
-    
-     
+    public static int v_green = 2;
+    GreenfootImage image = getImage();
      /**
      * Act - do whatever the vehicle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -26,20 +23,22 @@ public class vehicle extends add_vehicle
      public void act() 
      {
          load_vehicle();
-
       }
      /**
-     * If a container touches the vehicle (this actor) it changes the actor to another color
+     * If a container is placed (collision) on a vehicle both will dissapear from the world.
+     * It will add a new vehicle and this function set a random colored vehicle
      */
      public void load_vehicle() 
      {
-        //MyWorld world = (MyWorld)getWorld();
         Actor red = getOneIntersectingObject(container_red.class);
-        Actor green = getOneIntersectingObject(container_green.class); 
+        Actor green = getOneIntersectingObject(container_green.class);
         Actor blue = getOneIntersectingObject(Container.class);
+        /**
+         * If there is still a container in every color, set random colored truck
+         * 
+         */
         if (v_blue > 0 && v_red > 0 && v_green > 0)
         {
-
             if (vRandom == 0) 
               {  
                setImage("Truck_red.png"); 
@@ -49,7 +48,6 @@ public class vehicle extends add_vehicle
                  touchcontainer();
                  v_red = v_red-1;
                }
-              
             }
             if (vRandom == 1) 
               {
@@ -74,9 +72,112 @@ public class vehicle extends add_vehicle
               
             }
 
-          }
-     }
-    
+        }
+        else if (v_blue > 0 && v_red > 0 && v_green == 0)
+        {
+             if (vRandom1 == 0) 
+              {  
+               setImage("Truck_red.png"); 
+               vColor = "red";
+               if (vColor == container_red.vmg2_Red && red != null)
+               {
+                 touchcontainer();
+                 v_red = v_red-1;
+               }
+              
+            }
+            if (vRandom1 == 1) 
+              {
+               setImage("Truck_blue.png");
+               vColor = "blue";
+               if (vColor == Container.vmg2_Blue && blue != null)
+               {
+                 touchcontainer();
+                 v_blue = v_blue-1;
+               }
+              
+            }
+        }
+        else if (v_blue == 0 && v_red > 0 && v_green > 0)
+        {
+            if (vRandom1 == 0) 
+              {  
+               setImage("Truck_red.png"); 
+               vColor = "red";
+               if (vColor == container_red.vmg2_Red && red != null)
+               {
+                 touchcontainer();
+                 v_red = v_red-1;
+               }
+              
+            }
+            if (vRandom1 == 1) 
+              {
+               setImage("Truck_green.png");
+               vColor = "green";
+               if (vColor == container_green.vmg2_Green && green != null)
+               {
+                 touchcontainer();
+                 v_green = v_green-1;
+               }
+              
+            }
+        }
+        else if (v_blue > 0 && v_red == 0 && v_green > 0)
+        {
+            if (vRandom1 == 0) 
+              {  
+               setImage("Truck_green.png"); 
+               vColor = "green";
+               if (vColor == container_green.vmg2_Green && green != null)
+               {
+                 touchcontainer();
+                 v_green = v_green-1;
+               }
+              
+            }
+            if (vRandom1 == 1) 
+              {
+               setImage("Truck_blue.png");
+               vColor = "blue";
+                    if (vColor == Container.vmg2_Blue && blue != null)
+                    {
+                     touchcontainer();
+                     v_blue = v_blue-1;
+                    } 
+            }
+        }
+        else if (v_blue > 0 && v_red == 0 && v_green == 0)
+        {
+               setImage("Truck_blue.png");
+               vColor = "blue";
+                    if (vColor == Container.vmg2_Blue && blue != null)
+                    {
+                     touchcontainer();
+                     v_blue = v_blue-1;
+                    }   
+        }
+        else if (v_blue == 0 && v_red > 0 && v_green == 0)
+        {
+               setImage("Truck_red.png");
+               vColor = "red";
+                    if (vColor == container_red.vmg2_Red && red != null)
+                    {
+                     touchcontainer();
+                     v_red = v_red-1;
+                    }   
+        }
+        else if (v_blue == 0 && v_red == 0 && v_green > 0)
+        {
+               setImage("Truck_green.png");
+               vColor = "green";
+                    if (vColor == container_green.vmg2_Green && green != null)
+                    {
+                     touchcontainer();
+                     v_green = v_green-1;
+                    }   
+                }
+      }
      /**
      * If a container touches the vehicle (this actor) it deletes the container from the world.
      */
@@ -87,8 +188,7 @@ public class vehicle extends add_vehicle
          {  
           getWorld().removeObject(b);
           getWorld().removeObject(this);
-          vRandom =  Greenfoot.getRandomNumber(1);
-          add_vehicle.vmg2_test123 = 1;
+          add_vehicle.vmg2_addvehicle = 1;
        }
      }
     
